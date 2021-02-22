d6823fb901c11fd59324ff7199d17c7c22141960
Alejandra Rubio <rubioalejandraf@gmail.com>
2021-02-22 17:22:32 -0300

    primer desarrollo

---
+++ app3.js
@@ -0,0 +1,12 @@
+console.log('inicio de script');
+
+setTimeout(() => {
+    console.log('primer timeot');    
+}, 5000);
+setTimeout(() => {
+    console.log('segundo timeot');    
+}, 0);
+setTimeout(() => {
+    console.log('tercer timeot');    
+}, 0);
+console.log('fin de script');
